6ed24951fbe962ff652b01308b86345dab7ef32e
Owain Perry <owain.perry@thetrainline.com>
2012-06-20 21:01:10 +0100

    fixed ilmerge

--- default.ps1
+++ default.ps1
@@ -5,15 +5,15 @@ properties {
   $Build_Solution = 'SqlToGraphite.sln'
   $Build_Configuration = 'Release'
   $Build_Artifacts = 'output'
-  $fullPath="src\SqlToGraphite.host\output"
+  $fullPath= 'src\SqlToGraphite.host\output'
 }
 
 task default -depends Package
 
 task Test -depends Compile, Clean, StartOracle { 
  # write-output "hello"
-  Exec { packages\NUnit.Runners.2.6.0.12051\tools\nunit-console-x86.exe /nologo /config=SqlToGraphite.UnitTests.dll.config test\SqlToGraphite.UnitTests\Bin\x86\Release\SqlToGraphite.UnitTests.dll }
-  Exec { packages\NUnit.Runners.2.6.0.12051\tools\nunit-console-x86.exe .\test\SqlToGraphite.Host.UnitTests\bin\Release\SqlToGraphite.Host.UnitTests.dll }
+  Exec { packages\NUnit.Runners.2.6.0.12051\tools\nunit-console-x86.exe /nologo /config=SqlToGraphite.UnitTests.dll.config test\SqlToGraphite.UnitTests\output\SqlToGraphite.UnitTests.dll }
+  Exec { packages\NUnit.Runners.2.6.0.12051\tools\nunit-console-x86.exe .\test\SqlToGraphite.Host.UnitTests\output\SqlToGraphite.Host.UnitTests.dll }
 }
 
 task Compile -depends Clean { 
@@ -21,18 +21,20 @@ task Compile -depends Clean {
 }
 
 task Clean {   
+  rmdir $Build_Artifacts -Force -Recurse;
   Exec {  C:\Windows\Microsoft.NET\Framework\v4.0.30319\MSBuild.exe /p:OutDir=""$Build_Artifacts\"" /t:Clean $Build_Solution }  
 }
 
-
-task Ilmerge -depends Test {
-	Exec { tools\ilmerge /t:exe /out:output\sqlToGraphite.exe /targetplatform:v4 "$fullPath\SqlToGraphite.host.exe" "$fullPath\Graphite.dll" "$fullPath\log4net.dll" "$fullPath\SqlToGraphite.dll" "$fullPath\Topshelf.dll"};
-	Copy-Item  $fullPath\SqlToGraphite.host.exe.config  output;
+task Ilmerge -depends Test  {
+    mkdir $Build_Artifacts;
+	#$var = "" + "$fullPath" + "" + "$fullPath" + "\log4net.dll " + "$fullPath" + "\SqlToGraphite.dll " + "$fullPath" + "\Topshelf.dll";
+	#Write-Host $var;
+	Exec { tools\ilmerge.exe /closed /t:exe /out:output\sqlToGraphite.exe /targetplatform:v4 src\SqlToGraphite.host\output\SqlToGraphite.host.exe src\SqlToGraphite.host\output\Graphite.dll  src\SqlToGraphite.host\output\Topshelf.dll src\SqlToGraphite.host\output\log4net.dll };
+	Copy-Item  $fullPath\app.config.Template output\SqlToGraphite.exe.config;
 }
 
 task Package -depends Ilmerge {
 }
-
 
 task StartOracle {
 	 Start-Service "OracleServiceXE";
