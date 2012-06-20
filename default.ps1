746e143e2f8c239e3182dcebb30cfe0b441080f3
Owain Perry <owain.perry@thetrainline.com>
2012-06-20 21:24:07 +0100

    added assembly athcing

--- default.ps1
+++ default.ps1
@@ -6,41 +6,108 @@ properties {
   $Build_Configuration = 'Release'
   $Build_Artifacts = 'output'
   $fullPath= 'src\SqlToGraphite.host\output'
+  $version = '1.1.0.0'
 }
 
 task default -depends Package
 
-task Test -depends Compile, Clean, StartOracle { 
- # write-output "hello"
+task Test -depends Init, Compile, Clean, StartOracle {  
   Exec { packages\NUnit.Runners.2.6.0.12051\tools\nunit-console-x86.exe /nologo /config=SqlToGraphite.UnitTests.dll.config test\SqlToGraphite.UnitTests\output\SqlToGraphite.UnitTests.dll }
   Exec { packages\NUnit.Runners.2.6.0.12051\tools\nunit-console-x86.exe .\test\SqlToGraphite.Host.UnitTests\output\SqlToGraphite.Host.UnitTests.dll }
 }
 
-task Compile -depends Clean { 
+task Compile -depends  Clean { 
    Exec {  C:\Windows\Microsoft.NET\Framework\v4.0.30319\MSBuild.exe /p:OutDir=""$Build_Artifacts\"" /t:Rebuild /p:Configuration=$Build_Configuration $Build_Solution }
 }
 
 task Clean {   
-  rmdir $Build_Artifacts -Force -Recurse;
+  #rmdir $Build_Artifacts -Force -Recurse;
   Exec {  C:\Windows\Microsoft.NET\Framework\v4.0.30319\MSBuild.exe /p:OutDir=""$Build_Artifacts\"" /t:Clean $Build_Solution }  
+}
+
+task Init {
+    Generate-Assembly-Info `
+        -file "src\SqlToGraphite.host\Properties\AssemblyInfo.cs" `
+        -title "SqlToGraphite $version" `
+        -description "Graphite Service for collecting metrics" `
+        -company "peek.org.uk" `
+        -product "SqlToGraphite $version" `
+        -version $version `
+        -copyright "PerryOfPeek 2012"
+        
+     Generate-Assembly-Info `
+        -file "src\SqlToGraphite\Properties\AssemblyInfo.cs" `
+        -title "SqlToGraphite $version" `
+        -description "Graphite Service for collecting metrics" `
+        -company "peek.org.uk" `
+        -product "SqlToGraphite $version" `
+        -version $version `
+        -copyright "PerryOfPeek 2012"    
 }
 
 task Ilmerge -depends Test  {
     mkdir $Build_Artifacts;
-	#$var = "" + "$fullPath" + "" + "$fullPath" + "\log4net.dll " + "$fullPath" + "\SqlToGraphite.dll " + "$fullPath" + "\Topshelf.dll";
-	#Write-Host $var;
-	Exec { tools\ilmerge.exe /closed /t:exe /out:output\sqlToGraphite.exe /targetplatform:v4 src\SqlToGraphite.host\output\SqlToGraphite.host.exe src\SqlToGraphite.host\output\Graphite.dll  src\SqlToGraphite.host\output\Topshelf.dll src\SqlToGraphite.host\output\log4net.dll };
-	Copy-Item  $fullPath\app.config.Template output\SqlToGraphite.exe.config;
+    #$var = "" + "$fullPath" + "" + "$fullPath" + "\log4net.dll " + "$fullPath" + "\SqlToGraphite.dll " + "$fullPath" + "\Topshelf.dll";
+    #Write-Host $var;
+    Exec { tools\ilmerge.exe /closed /t:exe /out:output\sqlToGraphite.exe /targetplatform:v4 src\SqlToGraphite.host\output\SqlToGraphite.host.exe src\SqlToGraphite.host\output\Graphite.dll  src\SqlToGraphite.host\output\Topshelf.dll src\SqlToGraphite.host\output\log4net.dll };
+    Copy-Item  $fullPath\app.config.Template output\SqlToGraphite.exe.config;
 }
 
 task Package -depends Ilmerge {
 }
 
 task StartOracle {
-	 Start-Service "OracleServiceXE";
-	 Start-Service "OracleXETNSListener";
+     Start-Service "OracleServiceXE";
+     Start-Service "OracleXETNSListener";
 }
 
 task ? -Description "Helper to display task info" {
-	Write-Documentation
+    Write-Documentation
+}
+
+function Get-Git-Commit
+{
+    $gitLog = git log --oneline -1
+    return $gitLog.Split(' ')[0]
+}
+
+function Generate-Assembly-Info
+{
+param(
+    [string]$clsCompliant = "true",
+    [string]$title, 
+    [string]$description, 
+    [string]$company, 
+    [string]$product, 
+    [string]$copyright, 
+    [string]$version,
+    [string]$file = $(Throw "file is a required parameter.")
+)
+  $commit = Get-Git-Commit
+  $asmInfo = "using System;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: CLSCompliantAttribute($clsCompliant)]
+[assembly: ComVisibleAttribute(false)]
+[assembly: AssemblyTitleAttribute(""$title"")]
+[assembly: AssemblyDescriptionAttribute(""$description"")]
+[assembly: AssemblyCompanyAttribute(""$company"")]
+[assembly: AssemblyProductAttribute(""$product"")]
+[assembly: AssemblyCopyrightAttribute(""$copyright"")]
+[assembly: AssemblyVersionAttribute(""$version"")]
+[assembly: AssemblyInformationalVersionAttribute(""$version / $commit"")]
+[assembly: AssemblyFileVersionAttribute(""$version"")]
+[assembly: AssemblyDelaySignAttribute(false)]
+"
+
+    $dir = [System.IO.Path]::GetDirectoryName($file)
+    if ([System.IO.Directory]::Exists($dir) -eq $false)
+    {
+        Write-Host "Creating directory $dir"
+        [System.IO.Directory]::CreateDirectory($dir)
+    }
+    Write-Host "Generating assembly info file: $file"
+    out-file -filePath $file -encoding UTF8 -inputObject $asmInfo
 }
--- src/SqlToGraphite/Properties/AssemblyInfo.cs
+++ src/SqlToGraphite/Properties/AssemblyInfo.cs
@@ -1,36 +1,17 @@
+ï»¿using System;
 using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-// General Information about an assembly is controlled through the following 
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
-[assembly: AssemblyTitle("SessionServer")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("TheTrainLine.com")]
-[assembly: AssemblyProduct("SessionServer")]
-[assembly: AssemblyCopyright("Copyright © TheTrainLine.com 2010")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
+[assembly: CLSCompliantAttribute(true)]
+[assembly: ComVisibleAttribute(false)]
+[assembly: AssemblyTitleAttribute("SqlToGraphite 1.1.0.0")]
+[assembly: AssemblyDescriptionAttribute("Graphite Service for collecting metrics")]
+[assembly: AssemblyCompanyAttribute("peek.org.uk")]
+[assembly: AssemblyProductAttribute("SqlToGraphite 1.1.0.0")]
+[assembly: AssemblyCopyrightAttribute("PerryOfPeek 2012")]
+[assembly: AssemblyVersionAttribute("1.1.0.0")]
+[assembly: AssemblyInformationalVersionAttribute("1.1.0.0 / 6ed2495")]
+[assembly: AssemblyFileVersionAttribute("1.1.0.0")]
+[assembly: AssemblyDelaySignAttribute(false)]
 
-// Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
-// COM, set the ComVisible attribute to true on that type.
-[assembly: ComVisible(false)]
-
-// The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("6d6de73a-175a-400a-b8e4-1c152c06c9b2")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
