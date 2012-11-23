d67ddb9038b134b03fb0036a8cddeb75598ccb0c
Owain Perry <owain.perry@thetrainline.com>
2012-11-23 10:33:38 +0000

    Working and tested version of transaction counting and amounts

--- src/SqlToGraphite.host/Properties/AssemblyInfo.cs
+++ src/SqlToGraphite.host/Properties/AssemblyInfo.cs
@@ -5,13 +5,13 @@ using System.Runtime.InteropServices;
 
 [assembly: CLSCompliantAttribute(true)]
 [assembly: ComVisibleAttribute(false)]
-[assembly: AssemblyTitleAttribute("SqlToGraphite 0.3.0.5")]
+[assembly: AssemblyTitleAttribute("SqlToGraphite 0.3.0.6")]
 [assembly: AssemblyDescriptionAttribute("Graphite Service for collecting metrics")]
 [assembly: AssemblyCompanyAttribute("peek.org.uk")]
-[assembly: AssemblyProductAttribute("SqlToGraphite 0.3.0.5")]
+[assembly: AssemblyProductAttribute("SqlToGraphite 0.3.0.6")]
 [assembly: AssemblyCopyrightAttribute("PerryOfPeek 2012")]
-[assembly: AssemblyVersionAttribute("0.3.0.5")]
-[assembly: AssemblyInformationalVersionAttribute("0.3.0.5 / 388e997")]
-[assembly: AssemblyFileVersionAttribute("0.3.0.5")]
+[assembly: AssemblyVersionAttribute("0.3.0.6")]
+[assembly: AssemblyInformationalVersionAttribute("0.3.0.6 / ad81818")]
+[assembly: AssemblyFileVersionAttribute("0.3.0.6")]
 [assembly: AssemblyDelaySignAttribute(false)]
 
