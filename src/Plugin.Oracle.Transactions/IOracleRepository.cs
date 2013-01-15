7ae65d6f772b6712184c1ebb4215441ea17ef24b
Owain Perry <owain.perry@thetrainline.com>
2013-01-15 16:36:56 +0000

    removed deleted files

--- src/Plugin.Oracle.Transactions/IOracleRepository.cs
+++
@@ -1,9 +0,0 @@
-using System.Data;
-
-namespace Plugin.Oracle.Transactions
-{
-    public interface IOracleRepository
-    {
-        DataSet ExecuteQuery(string connectionString, string sql);
-    }
-}
