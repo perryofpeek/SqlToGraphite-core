70ace88442d65c5ac7ee5e5ebf9e67ae4d4a1c22
Owain Perry <owain.perry@thetrainline.com>
2013-02-17 10:09:48 +0000

    updated version number

--- src/Graphite/Properties/AssemblyInfo.cs
+++ src/Graphite/Properties/AssemblyInfo.cs
@@ -5,13 +5,13 @@ using System.Runtime.InteropServices;
 
 [assembly: CLSCompliantAttribute(true)]
 [assembly: ComVisibleAttribute(false)]
-[assembly: AssemblyTitleAttribute("SqlToGraphite 0.3.0.17")]
+[assembly: AssemblyTitleAttribute("SqlToGraphite 0.3.0.18")]
 [assembly: AssemblyDescriptionAttribute("Graphite Service for collecting metrics")]
 [assembly: AssemblyCompanyAttribute("peek.org.uk")]
-[assembly: AssemblyProductAttribute("SqlToGraphite 0.3.0.17")]
+[assembly: AssemblyProductAttribute("SqlToGraphite 0.3.0.18")]
 [assembly: AssemblyCopyrightAttribute("PerryOfPeek 2013")]
-[assembly: AssemblyVersionAttribute("0.3.0.17")]
-[assembly: AssemblyInformationalVersionAttribute("0.3.0.17 / 1ffc50a")]
-[assembly: AssemblyFileVersionAttribute("0.3.0.17")]
+[assembly: AssemblyVersionAttribute("0.3.0.18")]
+[assembly: AssemblyInformationalVersionAttribute("0.3.0.18 / 59c2395")]
+[assembly: AssemblyFileVersionAttribute("0.3.0.18")]
 [assembly: AssemblyDelaySignAttribute(false)]
 
